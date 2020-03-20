84eaf3973e8aa57f71b71b2dabff7edc0aff1a8f
HilmanMln4 <hilman.maulana71@ui.ac.id>
2020-03-20 19:56:44 +0700

    Modul 5 - Post Test

--- JFood.java
+++ JFood.java
@@ -15,30 +15,30 @@ public class JFood
 {    
     public static void main(String[] args)
     {
-        /*Location RSLahir = new Location("RS Fatmawati", "Jakarta", "Tempat lahir");
+        Location RSLahir = new Location("RS Fatmawati", "Jakarta", "Tempat lahir");
         Seller Jaya = new Seller(123, "Hilman", "hilmanmaulana145@ymail.com", "083894243504", RSLahir);
         Food Mekdi = new Food(444,"Ayam Bucket", 75000, FoodCategory.Western, Jaya);
         Food Kfc = new Food(445, "French Fries", 20000, FoodCategory.Western, Jaya);
-        Promo diskon = new Promo (111, "Post Test dapet 100", 50000, 50000, true);*/
+        Promo diskon = new Promo (111, "Post Test dapet 100", 50000, 50000, true);
         Customer Niko = new Customer(456, "Niko", "Nichooouiacid", "99999999", new GregorianCalendar(2019,02,20));
         Customer Nika = new Customer(457, "Nika", "nikaaa@ui.ac.id", "Hehehe12456", 2019, 02, 19);
         Customer Niki = new Customer(458, "Niki", "nikiii@ui.ac.id", "Hehehe12567");
         
        
-        System.out.println(Niko.toString());
+        /*System.out.println(Niko.toString());
         System.out.println(Nika.toString());
         System.out.println(Niki.toString());
         /*CashInvoice a=new CashInvoice(1, Mekdi, "1-1-2020", Niko, 5000, InvoiceStatus.Finished)*/
-        /*CashlessInvoice TapCash = new CashlessInvoice(222, Mekdi, "12 Maret 2020", Niko, InvoiceStatus.Ongoing);
-        CashlessInvoice eMoney = new CashlessInvoice(333, Kfc, "12 Maret 2020", Niko, InvoiceStatus.Ongoing, diskon);
-        CashlessInvoice Flazz = new CashlessInvoice(444, Mekdi, "12 Maret 2020", Niko, InvoiceStatus.Ongoing, diskon);
+        CashlessInvoice TapCash = new CashlessInvoice(222, Mekdi, Niko, InvoiceStatus.Ongoing);
+        CashlessInvoice eMoney = new CashlessInvoice(333, Kfc, Niko, InvoiceStatus.Ongoing, diskon);
+        CashlessInvoice Flazz = new CashlessInvoice(444, Mekdi, Nika, InvoiceStatus.Ongoing, diskon);
         //Invoice Bayar = new Invoice(444, Mekdi.getId(), "28 Februari 2019", Niko, Mekdi.getPrice(), InvoiceStatus.Finished);*/
-        /*CashInvoice rupiah = new CashInvoice(1, Mekdi, "13 Maret 2020", Niko, InvoiceStatus.Finished);
-        CashInvoice rupiaah = new CashInvoice(2, Kfc, "13 Maret 2020", Niko, InvoiceStatus.Finished, 5000);
+        CashInvoice rupiah = new CashInvoice(1, Kfc, Nika, InvoiceStatus.Finished, 0);
+        CashInvoice rupiaah = new CashInvoice(2, Mekdi, Niko, InvoiceStatus.Finished, 5000);
         rupiah.setTotalPrice();
         rupiaah.setTotalPrice();
-        rupiah.printData();
-        rupiaah.printData();*/
+        rupiah.toString();
+        rupiaah.toString();
         /*TapCash.setTotalPrice();
         eMoney.setTotalPrice();
         Flazz.setTotalPrice();
